51f9f54bc00997b9c41647d745c671068034f65c
Lucas Rodrigues <lukinhasr1423@gmail.com>
2022-08-22 22:03:25 -0300

    Criado a lógica do jogo

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,53 @@
-console.log('teste');
+let x = document.querySelector('.x');
+let o = document.querySelector('.o');
+let boxes = document.querySelectorAll('.box');
+let buttons = document.querySelectorAll('#buttons-container button');
+let messageContainer = document.querySelector('#message');
+let messageText = document.querySelector('#message p');
+let secondPlayer;
+
+//contador de jogadas
+let player1 = 0;
+let player2 = 0;
+
+//add o evento de click aos boxes
+for(let i = 0; i < boxes.length; i++) {
+
+    //Quando alguem clica na caixa
+    boxes[i].addEventListener('click', function() {
+
+        let el = ckeckEl(player1, player2);
+
+        // verifica se já tem x ou o
+        if(this.childNodes.length == 0) {
+
+            let cloneEl = el.cloneNode(true);
+
+            this.appendChild(cloneEl);
+    
+            // computar jogada
+            if (player1 == player2) {
+                player1++;
+            } else {
+                player2++;
+            }
+
+        }
+
+    });
+
+}
+
+// ver quem vai jogar
+function ckeckEl(player1, player2) {
+
+    if (player1 == player2) {
+        // x
+        el = x;
+    } else {
+        // o
+        el = o;
+    }
+
+    return el;
+}
